df46e14089f4ada948cecc4507cfcf5b380c8cc2
Islam-vz7 <i.elserougi2994@student.leedsbeckett.ac.uk>
2023-10-05 17:03:49 +0100

    Update

--- Groups.py
+++ Groups.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+  
+def Group(Students, GS):
+  
+  numgroups = int(Students // GS)
+  remaining_students = Students % GS
+  print(f'There will be {numgroups} groups with {remaining_students} students left over.')
 
 if __name__ == '__main__':
 
-  Students = input('How many students? ')
-  GS = input('Required group size? ')
+  Students = int(input('How many students? '))
+  GS = int(input('Required group size? '))
+  Group(Students, GS)
   
---
+++ New_Greeting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+  name = input('Type your name: ')
+  if name == '':
+    print(f'Hello, Stranger')
+  else:
+    print(f'Hello, {name}')
+    
