7036ce56f18ee135918f2a31cec6867b3f6417f4
Islam-vz7 <i.elserougi2994@student.leedsbeckett.ac.uk>
2023-10-05 11:17:46 +0100

    New

---
+++ Groups.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+
+  Students = input('How many students? ')
+  GS = input('Required group size? ')
+  
--- Temprature.py
+++ Temprature.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-if __name__ == '__Temprature__':
+if __name__ == '__main__':
   celsius = float(input('Enter the temperature in Celsius: '))
   Fahrenheit = celsius * 1.8 + 32
   print(f'{celsius}C is equivalent to {Fahrenheit}F')
