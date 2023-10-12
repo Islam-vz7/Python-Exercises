1855b2fd0f0818727595b9fb7f11d1f6f87bd4a1
Islam-vz7 <i.elserougi2994@student.leedsbeckett.ac.uk>
2023-10-12 10:19:30 +0100

    Marks

--- 7-Marks.py
+++ 7-Marks.py
@@ -1,12 +1,31 @@
-mark_1 = int(input('Enter mark #1: '))
-mark_2 = int(input('Enter mark #2: '))
-mark_3 = int(input('Enter mark #3: '))
-mark_4 = int(input('Enter mark #4: '))
-mark_5 = int(input('Enter mark #5: '))
+from statistics import mean
 
-highest = max(mark_1, mark_2, mark_3, mark_4, mark_5)
-lowest = min(mark_1, mark_2, mark_3, mark_4, mark_5)
-mean = (mark_1 + mark_2 + mark_3 + mark_4 + mark_5) // 5
+
+#mark_1 = int(input('Enter mark #1: '))
+#mark_2 = int(input('Enter mark #2: '))
+#mark_3 = int(input('Enter mark #3: '))
+#mark_4 = int(input('Enter mark #4: '))
+#mark_5 = int(input('Enter mark #5: '))
+#
+#highest = max(mark_1, mark_2, mark_3, mark_4, mark_5)
+#lowest = min(mark_1, mark_2, mark_3, mark_4, mark_5)
+#mean = (mark_1 + mark_2 + mark_3 + mark_4 + mark_5) // 5
+#
+#print(f'The highest mark is: {highest}')
+#print(f'The lowest mark is: {lowest}')
+#print(f'The mean mark is: {mean}')
+#
+#
+
+number_of_marks = int(input('How many marks do you want to enter? '))
+marks = []
+for i in range(number_of_marks):
+    mark = int(input('Enter mark: '))
+    marks.append(mark)
+
+highest = max(marks)
+lowest = min(marks)
+mean = mean(marks)
 
 print(f'The highest mark is: {highest}')
 print(f'The lowest mark is: {lowest}')
