163384adf6582b5f9ed6fa7099448e464b10ad6b
Islam-vz7 <i.elserougi2994@student.leedsbeckett.ac.uk>
2023-10-14 18:19:55 +0100

    Range

---
+++ 8-Range_Check.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#number = int(input('Enter the Number: '))
+
+#def Validate_number(number):
+ # return 0 <= number <= 100
+
+#print(f'{Validate_number(number)}')
+
+number = input()
+i = 0
+while i < len(number):
+ print(i)
+ i = i + 1
+
